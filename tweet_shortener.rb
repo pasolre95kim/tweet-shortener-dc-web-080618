0723e9ebe39333d4345681631344137bc15321ca
Michelle Kim <pasolre95kim@gmail.com>
2018-07-24 16:04:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,8 +29,8 @@ def word_substituter(tweet)
   end
 
 
-def bulk_tweet_shortener(tweets)
-  tweets.each do |tweet|
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
     puts word_substituter(tweet)
   end
 end
