cb699e5bcd0dd323b08998196712eb08b7b5d92f
Michelle Kim <pasolre95kim@gmail.com>
2018-07-24 15:37:44 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def dictionary
+  replace = {
+    "hello" => "hi",
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+}
+end
+
+
+def word_substituter(tweet)
+  tweet_shortener = dictionary.split.keys do |word|
+  if dictionary.keys.include?(word.downcase)
